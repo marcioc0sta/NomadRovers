0e5f0f5bba9218f6715d05b890cf74dd32ebf529
marcioc0sta <marciocst2@gmail.com>
2020-09-27 16:09:28 -0300

    feat: add HeroUnit.vue

---
+++ src/components/hero-unit/HeroUnit.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hero-unit" />
+</template>
+
+<script>
+export default {
+name: "HeroUnit"
+}
+</script>
+
+<style scoped>
+.hero-unit {
+  background: url("../../assets/6037_msl_banner.jpg") 0 0 no-repeat;
+  background-size: cover;
+  height: 80vh;
+  overflow: hidden;
+  width: 100%;
+}
+
+.hero-unit:before {
+  background: #1e272e;
+  content: '';
+  display: block;
+  height: 80vh;
+  position: absolute;
+  opacity: .65;
+  left: 0;
+  right: 0;
+  z-index: 10;
+}
+</style>
--- src/views/main/Main.vue
+++ src/views/main/Main.vue
@@ -1,14 +1,17 @@
 <template>
   <Header />
+  <HeroUnit />
 </template>
 
 <script>
 import Header from '../../components/header/Header.vue'
+import HeroUnit from '../../components/hero-unit/HeroUnit.vue'
 
 export default {
   name: 'Main',
   components: {
-    Header
+    Header,
+    HeroUnit,
   },
 }
 </script>
