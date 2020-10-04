ed93dba93b55d7fccac25af5ac03eef4eea76f40
marcioc0sta <marciocst2@gmail.com>
2020-10-04 18:20:59 -0300

    chore: update package.json and improve http service indent

--- src/services/HttpService.js
+++ src/services/HttpService.js
@@ -3,11 +3,11 @@ import axios from 'axios'
 const withKey = axios.create()
 
 withKey.interceptors.request.use(config => {
-    config.params = {
-        api_key: `${import.meta.env.VITE_API_KEY}`
-    }
+  config.params = {
+    api_key: `${import.meta.env.VITE_API_KEY}`
+  }
 
-    return config
+  return config
 })
 
 export { withKey }
