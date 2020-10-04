e2cd8ec4669a2d18eabe0afacf2c1f4c07e80033
marcioc0sta <marciocst2@gmail.com>
2020-10-04 16:13:00 -0300

    feat: add arrival date

--- src/components/rover-data/RoverData.vue
+++ src/components/rover-data/RoverData.vue
@@ -1,22 +1,34 @@
 <template>
-  <div class="rover-data">
+  <div v-if="Object.entries(rover).length > 0" class="rover-data">
     <section>
       <Rocket :size="80" fill="#000" />
       <div>
-        <h2>Lauch Date</h2>
-        {{ rover }}
+        <h2>Launch Date</h2>
+        <p>{{ rover.launch_date }}</p>
       </div>
     </section>
+    <section>
+      <Arrival :size="60" fill="#000" />
+      <div>
+        <h2>Landing Date</h2>
+        <p>{{ rover.landing_date }}</p>
+      </div>
+    </section>
+  </div>
+  <div>
+    <code>{{ rover }}</code>
   </div>
 </template>
 
 <script>
 import  Rocket from '../../components/icons/rocket.vue'
+import Arrival from '../../components/icons/arrival.vue';
 import {useStore} from "vuex";
 
 export default {
   name: "RoverData",
   components: {
+    Arrival,
     Rocket
   },
   store: useStore(),
@@ -31,10 +43,15 @@ export default {
 <style scoped>
   .rover-data {
     display: flex;
-    justify-content: space-between;
+    justify-content: space-around;
     padding: 30px 60px;
   }
   section {
+    align-items: center;
     display: flex;
+    text-align: left;
+  }
+  section svg {
+    margin-right: 10px;
   }
 </style>
