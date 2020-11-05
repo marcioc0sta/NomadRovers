a97e52703348f7d699a76f84211981749c52a9c0
marcioc0sta <marciocst2@gmail.com>
2020-11-05 20:40:05 -0300

    feat: add query area

--- src/components/query/Query.vue
+++ src/components/query/Query.vue
@@ -1,24 +1,40 @@
 <template>
 <div class="query-box">
+  <h2>Query for mars photos</h2>
+  <hr>
   <form>
     <div class="field">
       <label for="sun">
         Mars Sun
       </label>
-      <input id="sun" type="number">
+      <input placeholder="0" :max='rover.max_sol' min="0" id="sun" type="number">
     </div>
   </form>
 </div>
 </template>
 
 <script>
+import {useStore} from "vuex";
+
 export default {
-  name: "Query"
+  name: "Query",
+  store: useStore(),
+  computed: {
+    rover() {
+      return this.$store.state.activeRover
+    }
+  },
 }
 </script>
 
 <style scoped>
 .query-box {
+  margin-top: 30px;
+  text-align: left;
   width: 100%;
+}
+
+.query-box form {
+  margin-top: 15px;
 }
 </style>
