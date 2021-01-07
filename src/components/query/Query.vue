7fcbd4de666182728445d3dbbd14abb4daa0ba7f
marcioc0sta <marciocst2@gmail.com>
2021-01-07 19:02:10 -0300

    feat: add camera selector

--- src/components/query/Query.vue
+++ src/components/query/Query.vue
@@ -2,13 +2,30 @@
 <div class="query-box">
   <h2>Query for mars photos</h2>
   <hr>
-  <form>
+  <form @submit.prevent="submitForm">
     <div class="field">
       <label for="sun">
         Mars Sun
       </label>
-      <input placeholder="0" :max='rover.max_sol' min="0" id="sun" type="number">
+      <input
+          v-model="marsSun"
+          placeholder="0"
+          :max='rover.max_sol'
+          min="0" id="sun" type="number"
+      />
     </div>
+    <div class="field">
+      <label for="sun">
+        Select an Camera
+      </label>
+      <select v-model="selectedCam" name="camSelect" id="camSelect">
+        <option value="" disabled selected>Select a camera</option>
+        <option v-for="cam in rover.cameras" :key="cam.name" :value="cam.name">
+          {{ cam.full_name }}
+        </option>
+      </select>
+    </div>
+    <button type="submit">Query Photos</button>
   </form>
 </div>
 </template>
@@ -22,8 +39,21 @@ export default {
   computed: {
     rover() {
       return this.$store.state.activeRover
+    },
+  },
+  methods: {
+    submitForm() {
+      const { selectedCam, marsSun } = this
+      console.log(selectedCam.toLowerCase())
+      console.log(marsSun)
     }
   },
+  data() {
+    return {
+      marsSun: '0',
+      selectedCam: ''
+    }
+  }
 }
 </script>
 
